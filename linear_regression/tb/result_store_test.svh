c0b0b12bbd0d762eea82f790f0b15ffdeb744c35
derza-essien <d.erzaessien@outlook.com>
2026-08-03 16:02:36 +0100

    [update] draft makefile and complete testing template for result store

--- linear_regression/tb/result_store_test.svh
+++ linear_regression/tb/result_store_test.svh
@@ -1,24 +1,51 @@
+// Testbench for result_store.sv
 
-    // Force synchronous clocks to eliminate multi-clock translation glitches
-    //always_comb assume(clka == clkb);
+// specific address to test (anyconst ensures one random value is chosen)
+(* anyconst *) logic [ADDR_WIDTH-1:0]   chosen_addr;
 
-    logic first_valid_a = 1'b0;
-    logic first_valid_b = 1'b0;
+logic target_written = 1'b0;
+logic [DATA_WIDTH-1:0]  target_data;
 
-    always_ff @(posedge clka) first_valid_a <= 1'b1;
-    always_ff @(posedge clkb) first_valid_b <= 1'b1;
+logic past_available = 1'b0;
+logic check_read     = 1'b0;
+logic first_cycle    = 1'b1;
 
-    // Port A test - writing into ram
-    always_ff @(posedge clka) begin
-        if (first_valid_a && $past(ena) && $past(wea)) begin
-            writing_into_ram: assert(ram[$past(addra)] == $past(dina));
-        end
+
+always_comb begin
+    // default values for the first cycle
+    if(first_cycle) begin
+        assume(doutb == '0);
+        assume(dina == '0);
+        assume(ena == '0);
+        assume(wea == '0);
+        assume(enb == '0);
     end
 
-    // Port B test - reading the output RAM
-    always_ff @(posedge clkb) begin
-        if (first_valid_b && $past(enb)) begin
-            reading_from_ram: assert(doutb == ram[$past(addrb)]);
-            data_read: cover(doutb != '0);
-        end
+    if (target_written) begin
+        assume(!(ena && wea && (addra == chosen_addr)));
     end
+end
+
+// Port A test - writing into ram
+always_ff @(posedge clka) begin
+    if (ena && wea && (addra == chosen_addr)) begin
+        target_written  <=  1'b1;
+        target_data     <=  dina;
+    end
+    if(target_written) begin
+        // check that the target data is the same as that written into the RAM
+        ram_state_match: assume(ram[chosen_addr] == target_data);
+    end
+end
+
+// Port B test - reading the output RAM
+always_ff @(posedge clkb) begin
+    first_cycle <=  1'b0;
+    // Only checks output data the cycle after it is written into (ensures there isnt a race condition/ correct timing)
+    check_read  <=  enb && (addrb == chosen_addr) && target_written;
+    if (check_read) begin
+        // Check that the output data is equal to that written into the ram
+        reading_from_ram: assert(doutb == target_data);
+        data_read: cover(doutb != '0);
+    end
+end
