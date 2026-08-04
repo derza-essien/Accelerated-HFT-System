12619898082775620209214f6d150b02f732abe1
derza-essien <d.erzaessien@outlook.com>
2026-08-04 12:15:20 +0100

    quick cover update

--- linear_regression/tb/sample_unpack_test.svh
+++ linear_regression/tb/sample_unpack_test.svh
@@ -2,6 +2,8 @@
 
 always_comb begin
     y_is_lsb: assert(y == num_samples[15:0]);
+
+    y_read: cover(y != 16'd0);
 end
 
 genvar i;
@@ -9,6 +11,7 @@ generate
     for(i = 0; i < NUM_PARAMS; i++) begin : gen_sva_1
         always_comb begin
             assert(x[i] == num_samples[(i+1)*16 + 15 : (i+1) * 16]);
+            cover(x[i] != 16'd0);
         end
     end
 endgenerate
